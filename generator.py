f4d30ae91ba9b6db527e55acbfc04f825a999ef6
alphabru <53165772+alphabru@users.noreply.github.com>
2019-09-06 14:30:29 +0800

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,4 +1,15 @@
+#https://www.liaoxuefeng.com/wiki/1016959663602400/1017318207388128
+
 #杨辉三角
+#https://blog.csdn.net/qq_37701443/article/details/82707526
+
+'''
+重点是这句：
+ p = [1] + [p[i]+p[i+1] for i in range(len(p)-1)] +[1]
+
+每一个新的list中间的部分，都等于上一行list的：第0个元素+第1个元素，第1个元素+第2个元素，第2个元素+第3个元素,.......
+'''
+
 
 #by function
 def tri(r):
